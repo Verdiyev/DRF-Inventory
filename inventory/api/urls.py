6b1072f66fb022fc2f7ec5e1feff7b01e014454c
Verdiyev Zulfugar <39241206+Verdiyev@users.noreply.github.com>
2022-03-26 13:44:47 +0400

    Update urls.py

--- inventory/api/urls.py
+++ inventory/api/urls.py
@@ -5,15 +5,15 @@ from . import views
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
    
-    #Product API's
+    #Product APIs
     path("products/", views.ProductList.as_view(), name="product_list"),
     path("products/<int:pk>/", views.ProductDetail.as_view(), name="product_detail"),
 
-    #Order API's'
+    #Order APIs
     path("orders/", views.OrderList.as_view(), name="order_list"),
     path("orders/<int:pk>", views.OrderDetail.as_view(), name="order_detail"),
 
-    #Order Product API's
+    #Order Product APIs
     path("orderproducts/", views.OrderProductList.as_view(), name="orderproduct_list"),
     path("orderproducts/<int:pk>", views.OrderProductDetail.as_view(), name="orderproduct_detail"),
     
